bce2fc63185451738672cb8afe38b2df08db9a25
agustinmacia <amaciadelrio@gmail.com>
2019-10-06 21:52:36 -0300

    Se modifica resolver y schema.graphql

--- data/resolvers.js
+++ data/resolvers.js
@@ -1,17 +1,6 @@
-class Cliente {
-    constructor(id, {nombre, apellido, empresa, emails, edad, tipo,pedidos}){
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.empresa = empresa;
-        this.emails = emails;
-        this.edad = edad;
-        this.tipo = tipo;
-        this.pedidos = pedidos;
-    }
-}
-
-const clientesDB = {};
+import mongoose from 'mongoose';
+import {Clientes} from './db';
+import { rejects } from 'assert';
 
 export const resolvers = {
     Query: {
@@ -20,10 +9,42 @@ export const resolvers = {
         }
     },
     Mutation: {
-        crearCliente : ({input}) => {
-            const id = require('crypto').randomBytes(10).toString('hex');
-            clientesDB[id] = input;
-            return new Cliente(id, input);
+        crearCliente : (root, {input}) => {
+            const nuevoCliente = new Clientes({
+                nombre : input.nombre,
+                apellido : input.apellido,
+                empresa : input.empresa,
+                emails : input.emails,
+                edad : input.edad,
+                tipo : input.tipo,
+                pedidos : input.pedidos
+            });
+
+            nuevoCliente.id = nuevoCliente._id;
+
+            return new Promise ((resolve, object) => {
+                nuevoCliente.save((error) => {
+                    if(error) rejects (error)
+                    else resolve(nuevoCliente)
+                })
+            })
+
+        },
+        actualizarCliente : (root, {input}) => {
+            return new Promise((resolve, object) =>{
+                Clientes.findOneAndUpdate({ _id : input.id}, input, {new : true}, (error, cliente) => {
+                    if(error) rejects(error)
+                    else resolve(cliente)
+                });
+            });
+        },
+        eliminarCliente : (root, {id}) => {
+            return new Promise ((resolve, object) => {
+                Clientes.findOneAndDelete({_id : id}, (error) => {
+                    if(error) rejects(error)
+                    else resolve("Se elimino correctamente")
+                })
+            })
         }
     }
 }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import {schema} from './data/schema';
+import resolvers from './data/resolvers';
 
 const application = express();
 
