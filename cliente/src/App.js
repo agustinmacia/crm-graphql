71ca5e5ed64a99d81112633e743fb1e927b3aa25
Agustin MacIa Del Rio <MACIADRA@redlink.com.ar>
2019-10-08 11:33:38 -0300

    Commits varios

---
+++ cliente/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+
+//Importo componentes
+import Header from './components/header';
+
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  onError : ({networkError, graphQLErrors}) => {
+    console.log('graphQLErrors', graphQLErrors);
+    console.log('networkError', networkError);
+  }
+})
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client= {client}>
+        <h1>Hola</h1>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
